ea778218246533e210309de8748118b75f03dfce
draju <draju@aptean.com>
2025-09-21 12:47:14 +0530

    fix: Change output configuration from 'server' to 'static' in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: node({ mode: 'standalone' }),
   integrations: [react(), tailwind()],
   server: { port: 4321 }
